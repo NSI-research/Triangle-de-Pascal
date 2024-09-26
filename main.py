df1488775a33dd36695a0a0bac45eb2a18dd7ba1
Lodjo28 <109769264+Lodjo28@users.noreply.github.com>
2024-09-26 20:56:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
 def Pascal(N, Liste=[]):
-    if N == 0:
-        Liste.append([1])
+    
+    if N == 0:            # Cas terminal
+        Liste.append([1]) # On pose le 1 du haut du triangle
         return Liste
-    Liste = Pascal(N-1, Liste)
-    Liste.append([1])
+    
+    Liste = Pascal(N-1, Liste) # Appel récursif
+    Liste.append([1])    # On pose le 1 de début de ligne
+    
     for i in range(len(Liste[N-1])-1):
-        Liste[-1].append(Liste[N-1][i]+Liste[N-1][i+1])
-    Liste[-1].append(1)
+        Liste[-1].append(Liste[N-1][i]+Liste[N-1][i+1])    # On génère les valeurs intermédiaires
+    
+    Liste[-1].append(1)   # On pose le 1 de fin de ligne
     return Liste
